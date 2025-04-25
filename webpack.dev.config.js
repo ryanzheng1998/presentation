7d5d7362fcd25b6e058051f3b00f90a7ca069538
Ryan <ryanzheng1998@gmail.com>
2025-04-25 20:22:00 +0800

    change firebase to github page

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,68 +1,69 @@
 const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.tsx',
+  entry: './src/index.tsx',
 
-    mode: "development",
+  mode: 'development',
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        publicPath: '/',
-    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath:
+      'https://ryanzheng1998.github.io/react-motion-parameters-chooser/presentation',
+  },
 
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/template.html'
-        })
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.css', '.png', '.txt', '.svg'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+
+      {
+        test: /\.(txt)$/i,
+        use: 'raw-loader',
+      },
+
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
     ],
-
-    devServer: {
-        historyApiFallback: true,
-    },
-
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.css', '.png', '.txt', '.svg']
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-            },
-
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                loader: 'source-map-loader',
-            },
-
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-
-            {
-                test: /\.(txt)$/i,
-                use: 'raw-loader',
-            },
-
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-            }
-        ]
-    },
+  },
 }
